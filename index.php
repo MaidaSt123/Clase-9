cb289bba401a359a23168d5b24b9f4d147fbfb54
MaidaSt123 <mmstephens@uc.cl>
2017-05-10 13:29:44 -0300

    Primera versión

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('header.php');?>
+<div class="inner cover">
+<h2 class="cover-heading">Elecciones presidenciales USA 2016</h2>
+<h2 class="lead">Resultados de las votaciones por estados de cada candidato</h2>
+<p class="lead">Hillary Clinton logró ganar el voto popular con el 48,03% (62.523.126) de los sufragios, contra el 47,01% (61.201.031) obtenido por Donald Trump. Sin embargo, perdió el voto en el sistema del colegio electoral, obteniendo 232 contra 290, necesitando 270 para ganar.</p>
+</div>
+<?php include('footer.php');?>
